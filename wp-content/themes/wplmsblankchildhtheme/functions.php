f9e8ac3143207a9e19021c020b8ab8dda2a183ce
Aric Sangchat <aricsangchat@Arics-MacBook-Pro.local>
2018-12-15 18:11:34 +0700

    slick cdn

--- wp-content/themes/wplmsblankchildhtheme/functions.php
+++ wp-content/themes/wplmsblankchildhtheme/functions.php
@@ -7,7 +7,7 @@ function meta_wp_enqueue_scripts() {
     wp_register_script( 'meta_custom_js', META_DIR . 'js/custom.js', array('jquery'), META_VER, true );
     wp_enqueue_script( 'meta_custom_js' );
 
-    wp_register_script( 'slickslider', META_DIR . '//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js', array('jquery'), META_VER, true );
+    wp_register_script( 'slickslider', '//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js', array('jquery'), META_VER, true );
     wp_enqueue_script( 'slickslider' );
 }
 
