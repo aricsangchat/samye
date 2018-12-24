407bfc5ab8a706c278837d547e516c82f3f57520
Aric Sangchat <aricsangchat@Arics-MacBook-Pro.local>
2018-12-24 11:56:00 +0700

    styling home

--- wp-content/themes/wplmsblankchildhtheme/page-home.php
+++ wp-content/themes/wplmsblankchildhtheme/page-home.php
@@ -47,41 +47,54 @@ if ( have_posts() ) : while ( have_posts() ) : the_post();
     </div>
 
     <div class="container">
-        <div class="row">
-            <div class="col-xs-12">
-                <h1>Tag Line Goes Here, Spreading the Vibe</h1>
-                <p>The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.</p>
-                <a href="#">Link</a>
-            </div>
-        </div>
-
-        <div class="row">
-            <div class="col-xs-12 col-md-6">
+        <div class="row default-text-block">
+            <div class="col-xs-12 col-sm-6">
                 <h2>Can't make the retreat? Check out our at home study programs</h2>
                 <p>The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.</p>
-                <a href="#">Link</a>
+                <a class="primary-btn" href="#">Link</a>
             </div>
-            <div class="col-xs-12 col-md-6">
-                <h2>Find a Dharma Group in your area for local Study & Practice</h2>
-                <p>The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.</p>
-                <a href="#">Link</a>
+            <div class="col-xs-12 col-sm-6">
+                <img src="http://placehold.jp/500x350.png" alt="...">
             </div>
         </div>
+    </div>
 
-        <div class="row">
-            <div class="col-xs-12">
-                <h2>Tag Line Goes Here, Spreading the Vibe</h2>
-                <img src="http://placehold.jp/1500x300.png" alt="...">
+    <div class="program-group-block">
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-12 col-sm-6">
+                    <h2>At home study programs</h2>
+                    <p>The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.</p>
+                    <div class="row">
+                        <div class="col-xs-12 col-sm-4">
+                            <img src="http://placehold.jp/500x350.png" alt="...">
+                        </div>
+                        <div class="col-xs-12 col-sm-4">
+                            <img src="http://placehold.jp/500x350.png" alt="...">
+                        </div>
+                        <div class="col-xs-12 col-sm-4">
+                            <img src="http://placehold.jp/500x350.png" alt="...">
+                        </div>
+                    </div>
+                    <div class="button-wrapper">
+                        <a class="secondary-btn" href="#">Link</a>
+                    </div>
+
+                </div>
+                <div class="col-xs-12 col-sm-6">
+                    <h2>At home study programs</h2>
+                    <p>The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.</p>
+                    <img src="http://placehold.jp/500x350.png" alt="...">
+                    <div class="button-wrapper">
+                        <a class="secondary-btn" href="#">Link</a>
+                    </div>
+                </div>
             </div>
         </div>
+    </div>
 
-        <div class="row">
-            <div class="col-xs-12">
-                <h2>Sign up Email Updates</h2>
-                <input type="text" />
-            </div>
-        </div>
 
+    <div class="container">
         <div class="row">
             <div class="col-xs-12 col-md-5">
                 <img src="http://placehold.jp/500x500.png" alt="...">
@@ -102,7 +115,9 @@ if ( have_posts() ) : while ( have_posts() ) : the_post();
                 <h2>Recent Post</h2>
             </div>
         </div>
+    </div>
 
+    <div class="container">
         <div class="row">
             <div class="col-xs-12">
                 <h2>Explore by Topic</h2>
@@ -135,7 +150,9 @@ if ( have_posts() ) : while ( have_posts() ) : the_post();
                 <h2>Topic</h2>
             </div>
         </div>
+    </div>
 
+    <div class="container">
         <div class="row">
             <div class="col-xs-12">
                 <h2>Guru Rinpoche Day Teachings</h2>
@@ -144,7 +161,7 @@ if ( have_posts() ) : while ( have_posts() ) : the_post();
             </div>
         </div>
 
-         <div class="row">
+            <div class="row">
             <div class="col-xs-12 col-sm-6">
                 <img src="http://placehold.jp/500x500.png" alt="...">
             </div>
@@ -154,7 +171,9 @@ if ( have_posts() ) : while ( have_posts() ) : the_post();
                 <a href="#">Link</a>
             </div>
         </div>
+    </div>
 
+    <div class="container">
         <div class="row">
             <div class="col-xs-12">
                 <h2>Weekly Reflections</h2>
@@ -162,6 +181,8 @@ if ( have_posts() ) : while ( have_posts() ) : the_post();
             </div>
         </div>
     </div>
+
+    
 
     <div class="<?php echo vibe_get_container(); ?>">
         <div class="row">
