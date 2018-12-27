fdad95aadfaca8d067903a5960a8f2f353291382
Aric Sangchat <aricsangchat@Arics-MacBook-Pro.local>
2018-12-27 17:11:38 +0700

    added fonts and acf

--- wp-content/themes/wplmsblankchildhtheme/page-home.php
+++ wp-content/themes/wplmsblankchildhtheme/page-home.php
@@ -12,39 +12,96 @@ if ( have_posts() ) : while ( have_posts() ) : the_post();
  
 ?>
 <section id="content">
-    <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
-        <!-- Indicators -->
-        <ol class="carousel-indicators">
-            <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-            <li data-target="#carousel-example-generic" data-slide-to="1"></li>
-        </ol>
+    <?php
 
-        <!-- Wrapper for slides -->
-        <div class="carousel-inner" role="listbox">
-            <div class="item active">
-                <div class="carousel-image" style="background-image: url(http://placehold.jp/1500x480.png)"></div>
-                <div class="carousel-caption">
-                    <p>Caption</p>
-                </div>
-            </div>
-            <div class="item">
-                <div class="carousel-image" style="background-image: url(http://placehold.jp/1500x480.png)"></div>
-                <div class="carousel-caption">
-                    <p>Caption</p>
-                </div>
-            </div>
-        </div>
+    // check if the flexible content field has rows of data
+    if( have_rows('flexible_layout') ):
 
-        <!-- Controls -->
-        <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
-            <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-            <span class="sr-only">Previous</span>
-        </a>
-        <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
-            <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-            <span class="sr-only">Next</span>
-        </a>
-    </div>
+        // loop through the rows of data
+        while ( have_rows('flexible_layout') ) : the_row();
+
+            if( get_row_layout() == 'hero_slider' ):
+                
+                if( have_rows('hero_slide') ):
+                    echo '    <div id="hero-carousel" class="carousel slide" data-ride="carousel">
+                    <!-- Indicators -->
+                    <ol class="carousel-indicators">
+                        <li data-target="#chero-carousel" data-slide-to="0" class="active"></li>
+                        <li data-target="#hero-carousel" data-slide-to="1"></li>
+                    </ol>
+            
+                    <!-- Wrapper for slides -->
+                    <div class="carousel-inner" role="listbox">';
+                    
+                    while ( have_rows('hero_slide') ) : the_row();
+                        if (get_row_index() === 1 ) {
+                            echo '<div class="item active">
+                            <div class="carousel-image" style="background-image: url('. get_sub_field('hero_image') .')">
+                                <div class="hero-caption">
+                                    <h2>'.get_sub_field('hero_title').'</h2>
+                                    '.get_sub_field('hero_text_area').'
+                                    <a class="primary-btn" href="'.get_sub_field('hero_button_url').'">'.get_sub_field('hero_button_text').'</a>
+                                </div>
+                            </div>
+                            </div>';
+                        } else {
+                            echo '<div class="item">
+                            <div class="carousel-image" style="background-image: url('. get_sub_field('hero_image') .')">
+                                <div class="hero-caption">
+                                    <h2>'.get_sub_field('hero_title').'</h2>
+                                    '.get_sub_field('hero_text_area').'
+                                    <a class="primary-btn" href="'.get_sub_field('hero_button_url').'">'.get_sub_field('hero_button_text').'</a>
+                                </div>
+                            </div>
+                            </div>';
+                        }
+                        
+
+                    endwhile;
+
+                    echo '        </div>
+
+                        <!-- Controls -->
+                        <a class="left carousel-control" href="#hero-carousel" role="button" data-slide="prev">
+                            <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+                            <span class="sr-only">Previous</span>
+                        </a>
+                        <a class="right carousel-control" href="#hero-carousel" role="button" data-slide="next">
+                            <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                            <span class="sr-only">Next</span>
+                        </a>
+                    </div>';
+
+                endif;
+
+            elseif( get_row_layout() == '2_column_content_default' ): 
+                echo '<div class="container">
+                <div class="row default-text-block">
+                    <div class="col-xs-12 col-sm-6">
+                        <h2>'.get_sub_field('title').'</h2>
+                        '.get_sub_field('text_area').'
+                        <a class="primary-btn" href="'.get_sub_field('button_url').'">'.get_sub_field('button_text').'</a>
+                    </div>
+                    <div class="col-xs-12 col-sm-6">
+                        <img src="'.get_sub_field('image')['url'].'" alt="'.get_sub_field('title').'">
+                    </div>
+                </div>
+            </div>';
+            elseif( get_row_layout() == 'download' ): 
+
+                $file = get_sub_field('file');
+
+            endif;
+
+        endwhile;
+
+    else :
+
+        // no layouts found
+
+    endif;
+
+    ?>
 
     <div class="container">
         <div class="row default-text-block">
@@ -67,14 +124,16 @@ if ( have_posts() ) : while ( have_posts() ) : the_post();
                     <p>The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.</p>
                     <div class="row">
                         <div class="col-xs-12 col-sm-4">
-                            <img src="<?php echo get_the_post_thumbnail_url( $recent[13862], 'full' )?>" alt="...">
-                            <h3><?php $post = get_post( 13862 ); echo $post->post_title;?></h3>
+                            <img src="<?php echo get_the_post_thumbnail_url( 17234, 'full' )?>" alt="<?php echo get_post_meta( get_post_thumbnail_id( 17234 ), '_wp_attachment_image_alt', true ); ?>">
+                            <h3><a href="<?php echo get_permalink( 17234 ); ?>"><?php $post = get_post( 17234 ); echo $post->post_title;?></a></h3>
                         </div>
                         <div class="col-xs-12 col-sm-4">
-                            <img src="http://placehold.jp/1000x700.png" alt="...">
+                            <img src="<?php echo get_the_post_thumbnail_url( 17234, 'full' )?>" alt="<?php echo get_post_meta( get_post_thumbnail_id( 17234 ), '_wp_attachment_image_alt', true ); ?>">
+                            <h3><a href="<?php echo get_permalink( 17234 ); ?>"><?php $post = get_post( 17234 ); echo $post->post_title;?></a></h3>
                         </div>
                         <div class="col-xs-12 col-sm-4">
-                            <img src="http://placehold.jp/1000x700.png" alt="...">
+                            <img src="<?php echo get_the_post_thumbnail_url( 17234, 'full' )?>" alt="<?php echo get_post_meta( get_post_thumbnail_id( 17234 ), '_wp_attachment_image_alt', true ); ?>">
+                            <h3><a href="<?php echo get_permalink( 17234 ); ?>"><?php $post = get_post( 17234 ); echo $post->post_title;?></a></h3>
                         </div>
                     </div>
                     <div class="button-wrapper">
@@ -98,7 +157,7 @@ if ( have_posts() ) : while ( have_posts() ) : the_post();
     <div class="container featured-post-block">
         <div class="row">
             <div class="col-xs-12">
-                <h2>Explore by Topic</h2>
+                <h2>Latest Learning Resources</h2>
             </div>
         </div>
         <div class="row">
@@ -121,25 +180,85 @@ if ( have_posts() ) : while ( have_posts() ) : the_post();
                 $recent_posts = wp_get_recent_posts($args);
                 foreach( $recent_posts as $recent ){
                     echo '<img src="'.get_the_post_thumbnail_url( $recent["ID"], 'full' ).'" alt="">';
-                    echo '<h2><a href="' . get_permalink($recent["ID"]) . '">'.$recent["post_title"].'</a></h2> ';
+                    echo '<h2><a href="' . get_permalink($recent["ID"]) . '">'.(__($recent["post_title"])).'</a></h2> ';
 
                 }
                 wp_reset_query();
             ?>
             </div>
             <div class="col-xs-12 col-md-4">
-                <img src="http://placehold.jp/500x200.png" alt="...">
-                <h2>Recent Videos</h2>
-                <img src="http://placehold.jp/500x200.png" alt="...">
-                <h2>Recent Videos</h2>
+                <?php
+                    $args = array(
+                        'orderby' => 'post_date',
+                        'order' => 'DESC',
+                        'post_type' => 'post',
+                        'posts_per_page' => 2,
+                        'post_status' => 'publish',
+                        'tax_query' => array(
+                            array(
+                                'taxonomy' => 'post_format',
+                                'field' => 'slug',
+                                'terms' => array('post-format-audio', 'post-format-video'),
+                                'operator' => 'IN'
+                            )
+                        )
+                    );
+                    // The Query
+                    $query1 = new WP_Query( $args );
+
+                    if ( $query1->have_posts() ) {
+                        // The Loop
+                        while ( $query1->have_posts() ) {
+                            $query1->the_post();
+                            echo '<h2><a href="'. get_the_permalink() .'">' . get_the_title() . '</a></h2>';
+                        }
+                        
+                        /* Restore original Post Data 
+                        * NB: Because we are using new WP_Query we aren't stomping on the 
+                        * original $wp_query and it does not need to be reset with 
+                        * wp_reset_query(). We just need to set the post data back up with
+                        * wp_reset_postdata().
+                        */
+                        wp_reset_postdata();
+                    }
+                ?>
             </div>
             <div class="col-xs-12 col-md-3">
-                <img src="http://placehold.jp/500x200.png" alt="...">
-                <h2>Recent Post</h2>
-                <img src="http://placehold.jp/500x200.png" alt="...">
-                <h2>Recent Post</h2>
-                <img src="http://placehold.jp/500x200.png" alt="...">
-                <h2>Recent Post</h2>
+                <?php
+                        $args = array(
+                            'orderby' => 'post_date',
+                            'order' => 'DESC',
+                            'post_type' => 'post',
+                            'posts_per_page' => 4,
+                            'post_status' => 'publish',
+                            'tax_query' => array(
+                                array(
+                                    'taxonomy' => 'post_format',
+                                    'field' => 'slug',
+                                    'terms' => array('post-format-audio', 'post-format-video'),
+                                    'operator' => 'NOT IN'
+                                )
+                            )
+                        );
+                        // The Query
+                        $query1 = new WP_Query( $args );
+
+                        if ( $query1->have_posts() ) {
+                            // The Loop
+                            while ( $query1->have_posts() ) {
+                                $query1->the_post();
+                                echo '<h2>' . get_the_title() . '</h2>';
+                            }
+                            
+                            /* Restore original Post Data 
+                            * NB: Because we are using new WP_Query we aren't stomping on the 
+                            * original $wp_query and it does not need to be reset with 
+                            * wp_reset_query(). We just need to set the post data back up with
+                            * wp_reset_postdata().
+                            */
+                            wp_reset_postdata();
+                        }
+                    ?>
             </div>
         </div>
         <div class="row">
