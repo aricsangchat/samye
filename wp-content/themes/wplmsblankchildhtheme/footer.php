81251504518d385deaa081985e14b32f5bfa1e4c
Aric Sangchat <aricsangchat@Arics-MacBook-Pro.local>
2019-01-04 15:23:43 +0700

    saving footer

--- wp-content/themes/wplmsblankchildhtheme/footer.php
+++ wp-content/themes/wplmsblankchildhtheme/footer.php
@@ -2,10 +2,52 @@
 if ( ! defined( 'ABSPATH' ) ) exit;
 ?>
 <footer>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-sm-4">
+                <h4>Our Mandala</h4>
+                <ul>
+                    <li><a target="_blank" href="http://www.phakchokrinpoche.org/">Phakchok Rinpoche’s Site</a></li>
+                    <li><a target="_blank" href="https://shedrub.org/">Ka-Nying Shedrub Ling</a></li>
+                    <li><a target="_blank" href="https://www.cglf.org/">Chokgyur Lingpa Foundation</a></li>
+                    <li><a target="_blank" href="https://www.lotuslightdharmainstitute.org/">Lotus Light Dharma Institute</a></li>
+                    <li><a target="_blank" href="https://lhaseylotsawa.org/">Lhasey Lotsawa Translations and Publications</a></li>
+                    <li><a target="_blank" href="https://radicallyhappy.org/">Radically Happy</a></li>
+                    <li><a target="_blank" href="https://akaracollection.com/">Akara Collection – Dharma Shop</a></li>
+                </ul>
+            </div>
+            <div class="col-xs-12 col-sm-4">
+                <h4>Quick Links</h4>
+                <ul>
+                    <li><a target="_blank" href="#">Dakini Day Digest</a></li>
+                    <li><a target="_blank" href="#">FAQ</a></li>
+                    <li><a target="_blank" href="#">Contact Us</a></li>
+                    <li><a target="_blank" href="#">Privacy Policy</a></li>
+                    <li><a target="_blank" href="#">Sitemap</a></li>
+                    <li><a target="_blank" href="#">Donate</a></li>
+                    <li><a target="_blank" href="#">Volunteer</a></li>
+                </ul>
+            </div>
+            <div class="col-xs-12 col-sm-4">
+                <h4>Follow Us on Social Media</h4>
+                <ul class="social-icon-menu">
+                    <li><a target="_blank" href="#"><img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/icon-fb.png" /></a></li>
+                    <li><a target="_blank" href="#"><img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/icon-vm.png" /></a></li>
+                    <li><a target="_blank" href="#"><img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/icon-yt.png" /></a></li>
+                    <li><a target="_blank" href="#"><img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/icon-ig.png" /></a></li>
+                </ul>
+                <h4>Select Language</h4>
+                <?php do_action( 'wpml_add_language_selector' ); ?>
+            </div>
+        </div>
+    </div>
     <div id="scrolltop">
         <a><i class="icon-arrow-1-up"></i><span><?php _e('top','vibe'); ?></span></a>
     </div>
 </footer>
+<div class="copyright">
+    <p>© 2018 Samye Institute · All Rights Reserved</p>
+</div>
 </div><!-- END PUSHER -->
 </div><!-- END MAIN -->
 	<!-- SCRIPTS -->
--- wp-content/themes/wplmsblankchildhtheme/page-home.php
+++ wp-content/themes/wplmsblankchildhtheme/page-home.php
@@ -1,16 +1,8 @@
 <?php
 global $post;
 get_header(vibe_get_header());
-
-// if ( have_posts() ) : while ( have_posts() ) : the_post();
-
 ?>
 
-<?php
-
-    $v_add_content = get_post_meta( $post->ID, '_add_content', true );
- 
-?>
 <section id="content">
     <?php
 
@@ -141,7 +133,6 @@ get_header(vibe_get_header());
             </div>
         </div>
     </div>
-
 
     <div class="container latest-learning-resources">
         <div class="row">
@@ -362,7 +353,6 @@ get_header(vibe_get_header());
         </div>
     </div>
 
-
     <div class="container weekly-reflection">
         <div class="row">
             <div class="col-xs-12">
@@ -400,25 +390,7 @@ get_header(vibe_get_header());
         </div>
     </div>
 
-    <div class="<?php echo vibe_get_container(); ?>">
-        <div class="row">
-            <div class="col-xs-12">
-
-                <!-- <div class="<?php echo $v_add_content;?> content"> -->
-                    <?php
-                        the_content();
-                        $page_comments = vibe_get_option('page_comments');
-                        if(!empty($page_comments))
-                            comments_template();
-                     ?>
-                <!-- </div> -->
-            </div>
-        </div>
-    </div>
 </section>
-<?php
-// endwhile;
-// endif; 
-?>
+
 <?php
 get_footer( vibe_get_footer() );
