a6e94430151c0818e718679a5520561bfafde47b
Aric Sangchat <aricsangchat@Arics-MBP.net.fpt>
2019-01-21 15:34:04 +0700

    updating page.php

---
+++ wp-content/themes/wplmsblankchildhtheme/right-sidebar-page.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Template Name: Right Sidebar Page
+ */
+
+get_header(vibe_get_header());
+if ( have_posts() ) : while ( have_posts() ) : the_post();
+
+$title=get_post_meta(get_the_ID(),'vibe_title',true);
+if(vibe_validate($title) || empty($title)){
+?>
+<section id="page-title" style="background-image: url(<?php echo get_stylesheet_directory_uri(); ?>/assets/images/bg-page-title.png);">
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+                <h2><?php the_title(); ?></h2>
+            </div>
+        </div>
+    </div>
+</section>
+<?php
+}
+?>
+<section id="content">
+    <div class="<?php echo vibe_get_container(); ?>">
+        <div class="row">
+            <div class="col-md-9 col-sm-8">
+                <div class="content">
+                    <?php
+                        the_content();
+                        $page_comments = vibe_get_option('page_comments');
+                        if(!empty($page_comments))
+                            comments_template();
+                     ?>
+                </div>
+                <?php
+                
+                endwhile;
+                endif;
+                ?>
+            </div>
+            <div class="col-md-3 col-sm-4">
+                <?php get_template_part( 'sidebar' ); ?>
+            </div>
+        </div>
+    </div>
+</section>
+<?php
+get_footer(vibe_get_footer());
+?>
